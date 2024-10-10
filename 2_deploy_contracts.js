6beda175bec06607f255ed598d7fcd1f13cf1134
Sean Watson <126288646+RP1-SeanW@users.noreply.github.com>
2024-10-09 20:03:57 -0700

    Add files via upload

---
+++ 2_deploy_contracts.js
@@ -0,0 +1,11 @@
+const MySVMContract = artifacts.require("MySVMContract");
+
+module.exports = function (deployer) {
+    deployer.deploy(MySVMContract, "Hello, SVM!", { gas: 5000000 });
+};
+
+
+
+
+
+
